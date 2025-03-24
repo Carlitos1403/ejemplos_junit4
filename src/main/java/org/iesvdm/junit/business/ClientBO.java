699cff5ccb50220f14ed417d6ce41fe38a61ca72
carlos <carlos@1-dam-9>
2025-03-24 14:03:49 +0100

    Comentario tarea 24/03/2025

--- src/main/java/org/iesvdm/junit/business/ClientBO.java
+++ src/main/java/org/iesvdm/junit/business/ClientBO.java
@@ -7,6 +7,10 @@ import org.iesvdm.junit.model.Amount;
 import org.iesvdm.junit.model.Product;
 
 public interface ClientBO {
+	
+	/*
+	 * Esto es para la tarea de 24/03/2025
+	 * */
 
 	Amount getClientProductsSum(List<Product> products)
 			throws DifferentCurrenciesException;
